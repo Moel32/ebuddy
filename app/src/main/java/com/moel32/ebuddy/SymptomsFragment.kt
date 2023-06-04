31431b89ce29b40f949c76756881b3d8c05a603f
Gilles NGONGANG <gilles.ngongang@gmail.com>
2023-06-04 23:02:38 +0200

    feat: implementation for symptoms to diseases prediction using Apimedic and allow type of symptoms

--- app/src/main/java/com/moel32/ebuddy/SymptomsFragment.kt
+++ app/src/main/java/com/moel32/ebuddy/SymptomsFragment.kt
@@ -1,77 +1,142 @@
 package com.moel32.ebuddy
 
-    import android.os.Bundle
-    import android.view.LayoutInflater
-    import android.view.View
-    import android.view.ViewGroup
-    import android.widget.ArrayAdapter
-    import androidx.fragment.app.Fragment
-    import com.google.android.material.snackbar.Snackbar
-    import com.moel32.ebuddy.databinding.FragmentSymptomsBinding
-    import com.moel32.ebuddy.helpers.SymptomChecker
-    import kotlinx.coroutines.CoroutineScope
-    import kotlinx.coroutines.Dispatchers
-    import kotlinx.coroutines.launch
-    import kotlinx.coroutines.withContext
-    import com.github.kittinunf.result.Result
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import com.moel32.ebuddy.databinding.FragmentSymptomsBinding
+import com.moel32.ebuddy.helpers.SymptomChecker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import com.github.kittinunf.result.Result
+import android.widget.*
+
 
 /**
  * A fragment for performing symptom checks using the Apimedic API.
  * Performs a symptom check using the Apimedic API and displays the results in a [TextView].
  */
-    class SymptomsFragment : Fragment() {
+class SymptomsFragment : Fragment() {
 
-        private lateinit var binding: FragmentSymptomsBinding
-        private val checker = SymptomChecker()
+    private val checker = SymptomChecker()
 
-        override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View {
-            binding = FragmentSymptomsBinding.inflate(inflater, container, false)
+    // Define the map of symptoms that the user can select from.
+    private val symptomMap = mapOf(
+        1 to "Fever",
+        2 to "Cough",
+        3 to "Shortness of breath",
+        4 to "Fatigue",
+        5 to "Headache",
+        6 to "Muscle or body aches",
+        7 to "New loss of taste or smell",
+        8 to "Sore throat",
+        9 to "Congestion or runny nose",
+        10 to "Nausea or vomiting",
+        11 to "Diarrhea"
+    )
 
-            binding.checkButton.setOnClickListener {
-                val symptoms = binding.symptomEditText.text.toString().split(",")
-                val age = binding.ageEditText.text.toString().toIntOrNull()
-                val sex = binding.sexEditText.text.toString()
-                if (symptoms.isNotEmpty()) {
-                    binding.resultTextView.text = "Checking symptoms..."
-                    CoroutineScope(Dispatchers.IO).launch {
-                        val result = checker.checkSymptoms(symptoms, age, sex)
-                        withContext(Dispatchers.Main) {
-                            when (result) {
-                                is Result.Success -> {
-                                    val issues = result.value.issues
-                                    if (issues.isNotEmpty()) {
-                                        val issueNames = issues.joinToString(", ") { it.name }
-                                        binding.resultTextView.text = "Possible health issues: $issueNames"
-                                    } else {
-                                        binding.resultTextView.text = "No health issues found"
-                                    }
+    // Define the IDs of the symptoms that the user selects.
+    private val symptomIds = mutableSetOf<Int>()
+
+    // Define the binding and UI elements.
+    private lateinit var binding: FragmentSymptomsBinding
+    private lateinit var symptomSpinner: Spinner
+    private lateinit var ageEditText: EditText
+    private lateinit var sexRadioGroup: RadioGroup
+    private lateinit var maleRadioButton: RadioButton
+    private lateinit var femaleRadioButton: RadioButton
+    private lateinit var searchButton: Button
+    private lateinit var resetButton: Button
+    private lateinit var resultView: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSymptomsBinding.inflate(inflater, container, false)
+
+        val view = binding.root
+
+        // Initialize the UI elements.
+        symptomSpinner = binding.symptomSpinner
+        ageEditText = binding.ageEditText
+        sexRadioGroup = binding.sexRadioGroup
+        maleRadioButton = binding.maleRadioButton
+        femaleRadioButton = binding.femaleRadioButton
+        searchButton = binding.searchButton
+        resetButton = binding.resetButton
+        resultView = binding.resultTextView
+
+        // Set up the symptom spinner.
+        val symptomAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, symptomMap.values.toList())
+        symptomSpinner.adapter = symptomAdapter
+        symptomSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // Add the selected symptom ID to the set.
+                val symptomId = symptomMap.keys.toList()[position]
+                symptomIds.add(symptomId)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do nothing.
+            }
+        }
+
+        searchButton.setOnClickListener {
+
+            // Get the age and sex inputs.
+            val age = ageEditText.text.toString().toIntOrNull()
+            val sex = when (sexRadioGroup.checkedRadioButtonId) {
+                R.id.male_radio_button -> "male"
+                R.id.female_radio_button -> "female"
+                else -> null
+            }
+
+            if (symptomIds.isNotEmpty()) {
+                binding.resultTextView.text = "Checking symptoms..."
+                CoroutineScope(Dispatchers.IO).launch {
+                    val selectedIds = symptomIds.toList();
+                    val result = checker.checkSymptoms(selectedIds, age, sex)
+                    withContext(Dispatchers.Main) {
+                        when (result) {
+                            is Result.Success -> {
+                                val issues = result.value.issues
+                                if (issues.isNotEmpty()) {
+                                    val issueNames = issues.joinToString(", ") { it.name }
+                                    resultView.text = "Possible health issues: $issueNames"
+                                } else {
+                                    resultView.text = "No health issues found"
                                 }
-                                is Result.Failure -> {
-                                    val error = result.error
-                                    binding.resultTextView.text = "Error: ${error.exception.message}"
-                                }
-                                else -> {
-                                    binding.resultTextView.text = "Unexpected result: $result"
-                                }
+                            }
+                            is Result.Failure -> {
+                                val error = result.error
+                                resultView.text = "Error: ${error.exception.message}"
+                            }
+                            else -> {
+                                resultView.text = "Unexpected result: $result"
                             }
                         }
                     }
-                } else {
-                    binding.resultTextView.text = "Please enter at least one symptom."
                 }
+            } else {
+                resultView.text = "Please enter at least one symptom."
             }
-
-            binding.resetButton.setOnClickListener {
-                binding.symptomEditText.text.clear()
-                binding.ageEditText.text.clear()
-                binding.sexEditText.text.clear()
-                binding.resultTextView.text = ""
-            }
-
-            return binding.root
         }
+
+        // Set up the reset button.
+        resetButton.setOnClickListener {
+            // Clear the symptom IDs and reset the UI.
+            symptomIds.clear()
+            symptomSpinner.setSelection(0)
+            ageEditText.setText("")
+            sexRadioGroup.clearCheck()
+        }
+
+        return binding.root
     }
+}
--- app/src/main/java/com/moel32/ebuddy/helpers/SymptomChecker.kt
+++ app/src/main/java/com/moel32/ebuddy/helpers/SymptomChecker.kt
@@ -18,7 +18,7 @@ import kotlinx.serialization.json.Json
 class SymptomChecker {
 
     private val baseUrl = "https://sandbox-healthservice.priaid.ch"
-    private var apiKey = ""
+    private var apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImthbWVyZ2VuaWVAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiIxMjQyNyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAyMy0wNi0wNCIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNjg1OTE4ODIwLCJuYmYiOjE2ODU5MTE2MjB9.zwQjbApj6wfcxEquHlWlZ6gA9AvbP54DAFtkvVaLZgY"
     private val language = "en-gb"
     private val format = "json"
 
@@ -26,22 +26,13 @@ class SymptomChecker {
     /**
      * Performs a symptom check using the Apimedic API.
      *
-     * @param symptoms A list of symptom names to check.
+     * @param symptomIds A list of symptom ids to check.
      * @param age The age of the patient in years.
      * @param sex The sex of the patient ("male" or "female").
      * @return A [Result] object containing either a [SymptomCheckResult] object or a [FuelError] object.
      */
-    fun checkSymptoms(symptoms: List<String>, age: Int?, sex: String?): Result<SymptomCheckResult, FuelError> {
-        apiKey = getToken("kamergenie@gmail.com", "W>U%i9N.mtdWH&\$").get()
-        val symptomIds = fetchSymptomIds(symptoms)
-        val url = "$baseUrl/diagnosis?&language=$language&symptoms=${symptomIds.joinToString(",")}&token=$apiKey"
-        val params = mutableListOf<Pair<String, String>>()
-        if (age != null) {
-            params.add("age" to age.toString())
-        }
-        if (sex != null) {
-            params.add("gender" to sex)
-        }
+    fun checkSymptoms(symptomIds: List<Int>, year_of_birth: Int?, gender: String?): Result<SymptomCheckResult, FuelError> {
+        val url = "$baseUrl/diagnosis?language=$language&gender=$gender&year_of_birth=$year_of_birth&symptoms=[${symptomIds.joinToString(",")}]&token=$apiKey"
         val (_, _, result) = url
             .httpGet()
             .responseString()
