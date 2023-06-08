9301ae6c46c5f25e70168efc08df5b3edd390ef3
Moel32 <moungangivan@gmail.com>
2023-06-08 13:24:12 +0100

    feat: updated the home fragment
    added some new code to the home fragment

--- app/src/main/java/com/moel32/ebuddy/HomeFragment.kt
+++ app/src/main/java/com/moel32/ebuddy/HomeFragment.kt
@@ -1,18 +1,15 @@
 package com.moel32.ebuddy
 
-import android.R
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.hover.sdk.actions.HoverAction
 import com.hover.sdk.api.Hover
+import com.hover.sdk.api.HoverConfigException
 import com.hover.sdk.api.HoverParameters
 import com.hover.sdk.permissions.PermissionActivity
 import com.moel32.ebuddy.databinding.FragmentHomeBinding
@@ -39,36 +36,50 @@ class HomeFragment : Fragment(){
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         val view = binding.root
+        //val Intent
 
         // Initialize the UI elements.
         permissions_button_label = binding.permissionsButton
         action_button_label = binding.actionButton
 
+        //startActivityForResult(Intent(this, PermissionActivity::class.java), 0)
+
        // Hover.initialize(activity?.applicationContext, this)
         permissions_button_label.setOnClickListener {
             //
-            val i = Intent(activity?.applicationContext, HomeFragment::class.java)
+            val i = Intent(activity?.applicationContext, PermissionActivity::class.java)
             startActivityForResult(i, 0)
         }
+
+        /*try {
+            Hover.requestActionChoice(
+                arrayOf<String>("66b8e56a", "df7d3993"),
+                object : Hover.ActionChoiceListener() {
+                    fun onActionChosen(actionID: String?) {
+                        val i = HoverParameters.Builder(this).request(actionID)
+                            .buildIntent()
+                        startActivityForResult(i, 0)
+                    }
+
+                    fun onCanceled() {
+                        Toast.makeText(activity, "You must choose a SIM card").show()
+                    }
+                },
+                activity
+            )
+        } catch (e: HoverConfigException) {
+        }*/
 
         action_button_label.setOnClickListener {
             //
             val i = HoverParameters.Builder(activity)
-                .request("YOUR_ACTION_ID") // Add your action ID here
+                .request("66b8e56a") // Add your action ID here
+                .request("df7d3993")
                 //                    .extra("YOUR_VARIABLE_NAME", "TEST_VALUE") // Uncomment and add your variables if any
                 .buildIntent()
             startActivityForResult(i, 0)
         }
 
         return binding.root
-    }
-
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
-
     }
 }
